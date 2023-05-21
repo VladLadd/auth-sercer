7955c32ab6afb704627987865770f4709e14a66c
Vlad <vladislav.osadchy@gmail.com>
2023-05-22 00:12:48 +0300

    add firebase db

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Пример базы данных пользователей
-users = {
-    "u1": {
-        "password": "pass1"
-    },
-    "user2": {
-        "password": "password2"
-    }
-}
+# Инициализация Firebase
 
+cred = credentials.Certificate('./workspace-auth-aebdb-firebase-adminsdk-z6u31-d49d1f10a3.json')
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://firebase-adminsdk-z6u31@workspace-auth-aebdb.iam.gserviceaccount.com'
+})
+
+# Получение ссылки на базу данных
+ref = db.reference('users')
 
 # Регистрация нового пользователя
 @app.route('/register', methods=['POST'])
@@ -19,11 +21,10 @@ def register():
     data = request.get_json()
     username = data['username']
     password = data['password']
-    if username in users:
+    if ref.child(username).get() is not None:
         return jsonify({"success": False, "message": "User already exists"}), 400
-    users[username] = {"password": password}
+    ref.child(username).set({"password": password})
     return jsonify({"success": True, "message": "User registered successfully"}), 201
-
 
 # Авторизация пользователя
 @app.route('/login', methods=['POST'])
@@ -31,12 +32,12 @@ def login():
     data = request.get_json()
     username = data['username']
     password = data['password']
-    if username not in users:
+    user = ref.child(username).get()
+    if user is None:
         return jsonify({"success": False, "message": "User does not exist"}), 401
-    if users[username]['password'] != password:
+    if user['password'] != password:
         return jsonify({"success": False, "message": "Invalid password"}), 401
     return jsonify({"success": True, "message": "User authenticated successfully"}), 200
-
 
 if __name__ == '__main__':
     app.run(debug=True)
